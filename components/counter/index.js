ac780165c4eaa99abc7cd87ae764374b0e23242b
huangjiaxi <trustfor@sina.cn>
2020-03-17 10:26:47 +0800

    week6

--- components/counter/index.js
+++ components/counter/index.js
@@ -1,10 +1,21 @@
-// components/counter/index.js
+import {Cart} from "../../models/cart";
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
-
+    count:{
+      type:Number,
+      value:Cart.SKU_MIN_COUNT
+    },
+    min:{
+      type:Number,
+      value:Cart.SKU_MIN_COUNT
+    },
+    max:{
+      type:Number,
+      value:Cart.SKU_MAX_COUNT
+    }
   },
 
   /**
@@ -18,6 +29,22 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    onOverStep(event){
+      const minOrMaxOut = event.detail.type;
+      if(minOrMaxOut === 'overflow_max'){
+        wx.showToast({
+          icon:'none',
+          duration:3000,
+          title:`超出最大购买数量`
+        })
+      }
+      if(minOrMaxOut === 'overflow_min'){
+        wx.showToast({
+          icon:'none',
+          duration:3000,
+          title:`最少购买数量${Cart.SKU_MIN_COUNT}件噢`
+        })
+      }
+    }
   }
 })
