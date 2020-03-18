0b95162d380bbbb695ab6491da428d0112a4e4b9
huangjiaxi <huangjiaxi@oak-inc.com>
2020-03-18 16:25:01 +0800

    week6 4-10 完成分类数据获取

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,5 +1,6 @@
 import {getSystemSize} from "../../utils/system"
 import {px2rpx} from "../../miniprogram_npm/lin-ui/utils/util"
+import { Categories } from "../../models/categories";
 
 Page({
 
@@ -7,13 +8,41 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    defaultRootId: 2
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: async function (options) {
+    this.setDyncmicSegmentHeight();
+    this.initCategoryData();
+  },
+
+  async initCategoryData(){
+    const categories = new Categories();
+    this.data.categories = categories;
+    await categories.getAll();
+
+    const roots = categories.getRoots();
+    const defaultRoot = this.getDefaultRoot(roots);
+    const currentSubs = categories.getSubs(defaultRoot.id);
+    this.setData({
+      roots,
+      currentSubs,
+      currentBannerImg:defaultRoot.img
+    })
+  },
+
+  getDefaultRoot(roots){
+    let defaultRoot = roots.find(r=> r.id === this.data.defaultRootId);
+    if(!defaultRoot){
+      defaultRoot = roots[0]
+    }
+    return defaultRoot
+  },
+
+  async setDyncmicSegmentHeight(){
     const res = await getSystemSize();
     const windowHeightRpx = px2rpx(res.windowHeight);
 
@@ -24,13 +53,6 @@ Page({
     this.setData({
       segHeight:h
     });
-    /*
-    rate
-
-          750rpx               x
-    ---------------- = --------------------
-    res.screenHeight     res.windowHeight
-    */
   },
 
   onGotoSearch(){
