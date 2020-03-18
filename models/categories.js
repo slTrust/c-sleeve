e441fd45e0044bf180f042fea78f04109cd1c1f9
huangjiaxi <huangjiaxi@oak-inc.com>
2020-03-18 16:40:53 +0800

    week6 4-11 二级分类的Grid显示 数据获取

--- models/categories.js
+++ models/categories.js
@@ -17,8 +17,8 @@ class Categories{
         return this.roots;
     }
 
-    getSubs(rootId){
-        return this.roots.find(r=>r.id === rootId)
+    getSubs(parentId){
+        return this.subs.filter(sub=>sub.parent_id === parentId)
     }
 }
 
